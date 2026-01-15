b50f3db8b4a552e4e7adb55f1d4a14380b1ea5ee
meedihkm <meedihkm20@gmail.com>
2026-01-15 12:03:45 +0200

    chore: nettoyage projet - garde uniquement dz_delivery + admin_app - archive les anciens fichiers dans _archive (non tracké)

--- apps/livreur_app/android/app/src/main/kotlin/com/dzdelivery/livreur/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.dzdelivery.livreur
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- check_transactions_schema.sql
+++
@@ -1,8 +0,0 @@
--- Vérifier le schéma de la table transactions
-SELECT 
-    column_name,
-    data_type,
-    is_nullable
-FROM information_schema.columns
-WHERE table_name = 'transactions'
-ORDER BY ordinal_position;
